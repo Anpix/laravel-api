5018b3998319ede388d44b02a4ebb3465253fc2f
João Vítor Dias <joaodias@noctus.org>
2016-11-01 19:49:54 -0200

    Sign up user

--- app/Units/Auth/Http/Controllers/RegisterController.php
+++ app/Units/Auth/Http/Controllers/RegisterController.php
@@ -2,37 +2,19 @@
 
 namespace LaravelApi\Units\Auth\Http\Controllers;
 
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use LaravelApi\Core\Unit\Http\Controller;
-use LaravelApi\User;
+use LaravelApi\Domains\Users\Models\User;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use Validator;
-use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
-    use RegistersUsers;
-
-    /**
-     * Where to redirect users after login / registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
     /**
      * Create a new controller instance.
-     *
-     * @return void
      */
     public function __construct()
     {
@@ -50,7 +32,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|min:6|confirmed',
+            'password' => 'required|min:6',
         ]);
     }
 
@@ -67,5 +49,38 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+    }
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        event(new Registered($user = $this->create($request->all())));
+
+        $this->guard()->login($user);
+
+        try {
+            $token = app('tymon.jwt.auth')->fromUser($user);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'error generating token'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return response()->json(['token' => $token], Response::HTTP_CREATED);
+    }
+
+    /**
+     *
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard();
     }
 }
