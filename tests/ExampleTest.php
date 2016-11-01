a9ed3324cbd792422c64545aecb6b591635084cc
João Vítor Dias <joaodias@noctus.org>
2016-11-01 21:14:31 -0200

    Tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,19 +1,94 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use \Illuminate\Http\Response;
+use \LaravelApi\Domains\Users\Models\User;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
-    public function testBasicExample()
+    protected $user = [
+        'name' => 'Fake User',
+        'email' => 'fake@email.test',
+        'password' => 'fakepassword',
+    ];
+
+    /** @test */
+    public function testRegisterWithoutData()
     {
-        $this->visit('/')
-             ->see('Laravel');
+        $this->json('POST', '/register')
+            ->assertResponseStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->seeJsonEquals([
+                'name' => ['The name field is required.'],
+                'email' => ['The email field is required.'],
+                'password' => ['The password field is required.']
+            ]);
+    }
+
+    /** @test */
+    public function testRegisterWithData()
+    {
+        $this->json('POST', '/register', $this->user)
+            ->seeJsonStructure(['token']);
+    }
+
+    public function testRegisterWithExistingEmail()
+    {
+        $this->json('POST', '/register', $this->user)
+            ->seeJsonEquals([
+                'email' => ['The email has already been taken.']
+            ]);
+    }
+
+    /** @test */
+    public function testLoginWithoutCredentials()
+    {
+        $this->json('POST', '/login')
+            ->assertResponseStatus(Response::HTTP_UNAUTHORIZED);
+    }
+
+    /** @test */
+    public function testLoginWithData()
+    {
+        $fields = collect($this->user)->only('email', 'password');
+        $this->json('POST', '/login', $fields->toArray())
+            ->seeJsonStructure(['token']);
+    }
+
+    /** @test */
+    public function testLoginWithWrongCredentials()
+    {
+        $fields = [
+            'email' => 'not@this.email',
+            'password' => 'NotThisPassword',
+        ];
+
+        $this->json('POST', '/login', $fields)
+            ->seeJsonEquals([
+                'error' => 'invalid_credentials',
+            ]);
+    }
+
+    /** @test */
+    public function testRequestWithoutAuth()
+    {
+        $this->json('GET', '/user')
+            ->seeJsonEquals([
+                'error' => 'Unauthenticated.',
+            ]);
+    }
+
+    /** @test */
+    public function testRequestWithAuth()
+    {
+        $user = User::where(['email' => $this->user['email']])->first();
+
+        $token = app('tymon.jwt.auth')->fromUser($user);
+
+        $this->json('GET', '/user?token=' . $token)
+            ->seeJsonContains([
+                'name' => $this->user['name'],
+                'email' => $this->user['email']
+            ]);
+
+        $user->delete();
     }
 }
